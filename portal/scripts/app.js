8b8b252af7514451460985da393433e11f142098
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2023-01-17 02:27:23 -0500

    async promise with

--- portal/scripts/app.js
+++ portal/scripts/app.js
@@ -2,7 +2,29 @@
     'use strict';
     
     const app = angular.module("ng-app", []);
-    app.controller("basicController",basicController)
+    app.controller("basicController", basicController)
+    .controller("asyncController",asyncController)
+
+    asyncController.$inject = ["$q"]
+    function asyncController($q) {
+        let as = this;
+        as.result = 0;
+        function add(a,b) {
+            let q = $q.defer();
+            setTimeout(() => {
+                q.resolve(a+b)
+            },100)
+            return q.promise;
+        }
+        let st = Date.now()
+        add(5, 7)
+            .then(res => {
+                as.result = res
+                as.elapsed = Date.now() - st;
+            })
+        .catch(er=>console.log(er))
+    }
+
 
     basicController.$inject = ["$http"]
     function basicController($http) {
