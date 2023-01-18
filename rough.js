b3180f08c26fc0e793e28a5187396fc4923e46b4
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2023-01-17 21:57:10 -0500

    created gitignore file

--- rough.js
+++ rough.js
@@ -1,7 +1,28 @@
-create table product(
-	id int not null auto_increment,
-    name varchar(128) not null,
-    size int not null default 0,
-    is_available boolean not null default false,
-    primary key (id)
-);
+// //this is for practice/test code
+// console.log("Line 1")
+
+// setTimeout(_ => console.log("Line 2 from setTimeout"), 0);
+
+// Promise.resolve().then(_=>console.log("Line 3 from promise"))
+
+// console.log("Line 4")
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+async function call() {
+    await delay(1000)
+    return Promise.resolve(()=>Date.now() - time)
+}
+let gg = async() => {
+    
+    let g1 =  call()
+    let g2 =  call()
+    
+    return Promise.all([g1, g2])
+}
+let time = Date.now();
+//console.log(gg())
+gg().then(([y, p]) => {
+        console.log(y())
+        console.log(p())
+    });
+console.log("at the end")
